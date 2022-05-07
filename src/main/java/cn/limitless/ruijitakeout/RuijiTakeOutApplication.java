bc048bea5dbd0ce94cccec92c94ffa582647fdb2
GnaixEuy <sgghcj@live.com>
2022-05-07 19:32:50 +0800

    :tada: 项目启动

---
+++ src/main/java/cn/limitless/ruijitakeout/RuijiTakeOutApplication.java
@@ -0,0 +1,13 @@
+package cn.limitless.ruijitakeout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuijiTakeOutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RuijiTakeOutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/limitless/ruijitakeout/RuijiTakeOutApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.limitless.ruijitakeout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuijiTakeOutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
