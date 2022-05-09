a9dc0c8b8705df644f54f32b9376793e5bc1ec73
GnaixEuy <sgghcj@live.com>
2022-05-09 16:16:14 +0800

    :wrench: 添加了redis 配置

--- src/main/java/cn/limitless/ruijitakeout/utils/SMSUtils.java
+++ src/main/java/cn/limitless/ruijitakeout/utils/SMSUtils.java
@@ -6,27 +6,35 @@ import com.aliyuncs.dysmsapi.model.v20170525.SendSmsRequest;
 import com.aliyuncs.dysmsapi.model.v20170525.SendSmsResponse;
 import com.aliyuncs.exceptions.ClientException;
 import com.aliyuncs.profile.DefaultProfile;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * <img src="http://blog.GnaixEuy.cn/wp-content/uploads/2021/08/bug.jpeg"/>
  * <p>
  * 项目： ruiji-TakeOut
  * 短信发送工具类
+ *
  * @author GnaixEuy
  * @date 2022/5/7
  * @see <a href="https://github.com/GnaixEuy"> GnaixEuy的GitHub </a>
  */
 public class SMSUtils {
 
+    @Value("${aliyun.accessKeyId}")
+    private static String ACCESSKEYID;
+    @Value("${aliyun.secret}")
+    private static String SECRET;
+
     /**
      * 发送短信
-     * @param signName 签名
+     *
+     * @param signName     签名
      * @param templateCode 模板
      * @param phoneNumbers 手机号
-     * @param param 参数
+     * @param param        参数
      */
-    public static void sendMessage(String signName, String templateCode,String phoneNumbers,String param){
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "", "");
+    public static void sendMessage(String signName, String templateCode, String phoneNumbers, String param) {
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", SMSUtils.ACCESSKEYID, SMSUtils.SECRET);
         IAcsClient client = new DefaultAcsClient(profile);
 
         SendSmsRequest request = new SendSmsRequest();
@@ -34,11 +42,11 @@ public class SMSUtils {
         request.setPhoneNumbers(phoneNumbers);
         request.setSignName(signName);
         request.setTemplateCode(templateCode);
-        request.setTemplateParam("{\"code\":\""+param+"\"}");
+        request.setTemplateParam("{\"code\":\"" + param + "\"}");
         try {
             SendSmsResponse response = client.getAcsResponse(request);
             System.out.println("短信发送成功");
-        }catch (ClientException e) {
+        } catch (ClientException e) {
             e.printStackTrace();
         }
     }
