528ec5e1af30d602317da1963d17ca0f0f644c00
GnaixEuy <sgghcj@live.com>
2022-05-09 08:17:53 +0800

    :bug: 修复冗余处理器

--- src/main/java/cn/limitless/ruijitakeout/handler/GlobalExceptionHandler.java
+++ src/main/java/cn/limitless/ruijitakeout/handler/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package cn.limitless.ruijitakeout.handler;
 
 
+import cn.limitless.ruijitakeout.exception.CustomException;
 import cn.limitless.ruijitakeout.vo.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -35,12 +36,26 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(SQLIntegrityConstraintViolationException.class)
     public Result<String> exceptionHandler(SQLIntegrityConstraintViolationException ex) {
         log.error(ex.getMessage());
+
         if (ex.getMessage().contains("Duplicate entry")) {
             String[] split = ex.getMessage().split(" ");
             String msg = split[2] + "已存在";
             return Result.error(msg);
         }
+
         return Result.error("未知错误");
+    }
+
+    /**
+     * 异常处理方法
+     *
+     * @return
+     */
+    @ExceptionHandler(CustomException.class)
+    public Result<String> exceptionHandler(CustomException ex) {
+        log.error(ex.getMessage());
+
+        return Result.error(ex.getMessage());
     }
 }
 
